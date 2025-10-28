7e6b16135490510a43921175abebed8e529e4c00
Julian Florez <forezjuli08@hotmail.com>
2025-10-27 21:19:28 -0500

    feat(API): add SeedUserTypes migration

---
+++ EjercicioLoginNeitekBackend/EjercicioLoginNeitekBackend/Migrations/20251028014611_SeedUserTypes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EjercicioLoginNeitekBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedUserTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserTypes",
+                columns: new[] { "Id", "Description", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Admin user", "Admin" },
+                    { 2, "Customer user", "Customer" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "UserTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
