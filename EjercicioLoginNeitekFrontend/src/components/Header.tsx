e2857d18c92803335d867de14ca2ae94b02a01b7
Julian Florez <forezjuli08@hotmail.com>
2025-10-28 13:17:37 -0500

    feat(frontend): update css in Header, Signup and Welcome components

--- EjercicioLoginNeitekFrontend/src/components/Header.tsx
+++ EjercicioLoginNeitekFrontend/src/components/Header.tsx
@@ -4,14 +4,14 @@ export default function Header() {
   const location = useLocation();
 
   return (
-    <header className="flex justify-between items-center p-4 bg-blue-600 text-white shadow">
+    <header className="flex justify-between items-center p-2 sm:px-4 sm:py-2 bg-blue-600 text-white shadow">
       <h1 className="text-xl font-semibold">
         <Link to="/" className="hover:text-gray-200 transition">
           Inicio
         </Link>
       </h1>
 
-      <nav className="flex gap-4">
+      <nav className="flex flex-col sm:flex-row gap-4">
         {location.pathname !== "/login" && (
           <Link to="/login" className="hover:underline">
             Iniciar Sesión
--- EjercicioLoginNeitekFrontend/src/pages/Welcome.tsx
+++ EjercicioLoginNeitekFrontend/src/pages/Welcome.tsx
@@ -8,20 +8,20 @@ export default function Welcome() {
 
       <main className="flex flex-col items-center justify-center flex-1 text-center p-10">
         <h2 className="text-3xl font-semibold mb-4">Bienvenid@ 👋</h2>
-        <p className="text-gray-600 mb-6">
+        <p className="text-gray-600 mb-6 text-sm sm:text-base">
           Regístrate para crear una cuenta o inicia sesión si ya tienes una.
         </p>
 
-        <div className="flex gap-4">
+        <div className="flex flex-col sm:flex-row gap-4">
           <Link
             to="/signup"
-            className="bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-700 transition"
+            className="flex items-center justify-center bg-blue-600 text-white px-1 py-1 sm:px-5 sm:py-2 text-sm sm:text-base rounded hover:bg-blue-700 transition"
           >
             Registro
           </Link>
           <Link
             to="/login"
-            className="border border-blue-600 text-blue-600 px-5 py-2 rounded hover:bg-blue-600 hover:text-white transition"
+            className="flex items-center justify-center border border-blue-600 text-blue-600 px-1 py-1 sm:px-5 sm:py-2 text-sm sm:text-base rounded hover:bg-blue-600 hover:text-white transition"
           >
             Iniciar Sesión
           </Link>
