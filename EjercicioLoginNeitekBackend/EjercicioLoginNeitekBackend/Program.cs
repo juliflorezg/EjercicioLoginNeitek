d54d936ed96284981b168fa8d31c5cb14160285e
Julian Florez <forezjuli08@hotmail.com>
2025-10-27 21:23:26 -0500

    feat(API): register services for Repository, Service and password hasher in Program.cs

--- EjercicioLoginNeitekBackend/EjercicioLoginNeitekBackend/Program.cs
+++ EjercicioLoginNeitekBackend/EjercicioLoginNeitekBackend/Program.cs
@@ -1,4 +1,7 @@
 using EjercicioLoginNeitekBackend.Models;
+using EjercicioLoginNeitekBackend.Repository;
+using EjercicioLoginNeitekBackend.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,11 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UserContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UsersConnection")));
+
+builder.Services.AddScoped<IRepository<User, Guid>, UserRepository>();
+builder.Services.AddScoped<IRepository<UserType, int>, UserTypeRepository>();
+builder.Services.AddKeyedScoped<IUserService, UserService>("UserService");
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
 builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
