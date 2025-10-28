9904adc9cf2d418ba2773b82595dc93bc593a56c
Julian Florez <forezjuli08@hotmail.com>
2025-10-28 09:27:38 -0500

    feat(frontend): add Login page

---
+++ EjercicioLoginNeitekFrontend/src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import { Form } from "../components/Form";
+import { loginSchema } from "../utils/validators";
+import { useAuth } from "../hooks/useAuth";
+import Logo from "../components/Logo";
+
+export default function Login() {
+  const { login } = useAuth();
+
+  const fields = [
+    { name: "email", label: "Email", type: "email" },
+    { name: "password", label: "Password", type: "password" },
+  ];
+
+  return (
+    <div className="p-0 sm:p-4 md:p-10 min-h-screen flex flex-col items-center justify-start sm:justify-center">
+      <Logo />
+      <div>
+        <h1 className="text-xl sm:text-2xl font-semibold mb-8 text-center">Iniciar Sesión</h1>
+        <Form
+          fields={fields}
+          schema={loginSchema}
+          onSubmit={login}
+        />
+      </div>
+    </div>
+  );
+}
--- EjercicioLoginNeitekFrontend/src/router/index.tsx
+++ EjercicioLoginNeitekFrontend/src/router/index.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Login from "../pages/Login";
+import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 
 const router = createBrowserRouter([
-  // { path: "/login", element: <Login /> },
+  { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
 ]);
 
