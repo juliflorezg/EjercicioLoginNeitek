5ecc4b00c864e3e7516ae90a99097bad63a1c286
Julian Florez <forezjuli08@hotmail.com>
2025-10-28 19:50:51 -0500

    feat(TESTS): add tests for non existing user and invalid credentials

--- EjercicioLoginNeitekBackend/EjercicioLoginNeitekBackend.Tests/AuthControllerTests.cs
+++ EjercicioLoginNeitekBackend/EjercicioLoginNeitekBackend.Tests/AuthControllerTests.cs
@@ -55,4 +55,71 @@ public class AuthControllerTests
         var response = Assert.IsType<Dictionary<string, object>>(okResult.Value! as IDictionary<string, object> ?? new Dictionary<string, object>());
         Assert.NotNull(response);
     }
+
+    [Fact]
+    public async Task Login_NonExistingUser_ReturnsUnauthorized()
+    {
+        // Arrange
+        var mockUserService = new Mock<IUserService>();
+        var mockJwtService = new Mock<IJwtService>();
+
+
+        mockUserService
+            .Setup(s => s.GetByEmail(It.Is<LoginDTO>(l => l.Email == "wrong@example.com")))
+            .Returns((UserDTO?)null);
+
+        var controller = new AuthController(mockUserService.Object, mockJwtService.Object);
+
+        var loginDto = new LoginDTO
+        {
+            Email = "wrong@example.com",
+            Password = "badpass"
+        };
+
+        // Act
+        var result = await controller.Login(loginDto);
+
+        // Assert
+        var badRequest = Assert.IsType<UnauthorizedObjectResult>(result);
+        Assert.Equal("Credenciales invalidas", badRequest.Value);
+    }
+
+    [Fact]
+    public async Task Login_InvalidCredentials_ReturnsUnauthorized()
+    {
+        // Arrange
+        var mockUserService = new Mock<IUserService>();
+        var mockJwtService = new Mock<IJwtService>();
+
+        var fakeUser = new UserDTO
+        {
+            Id = Guid.NewGuid(),
+            Email = "test@example.com",
+            UserTypeId = 1
+        };
+
+        mockUserService
+            .Setup(s => s.GetByEmail(It.Is<LoginDTO>(l => l.Email == "test@example.com")))
+            .Returns(fakeUser);
+
+        mockUserService
+            .Setup(s => s.ValidatePassword(It.IsAny<LoginDTO>()))
+            .Returns(true);
+
+
+        var controller = new AuthController(mockUserService.Object, mockJwtService.Object);
+
+        var loginDto = new LoginDTO
+        {
+            Email = "wrong@example.com",
+            Password = "badpass"
+        };
+
+        // Act
+        var result = await controller.Login(loginDto);
+
+        // Assert
+        var badRequest = Assert.IsType<UnauthorizedObjectResult>(result);
+        Assert.Equal("Credenciales invalidas", badRequest.Value);
+    }
 }
