fe0270b91609b5079b87b22438cfdc97a9dc935f
Julian Florez <forezjuli08@hotmail.com>
2025-10-28 23:12:43 -0500

    feat(Frontend): add env variable for azure deployment

--- EjercicioLoginNeitekFrontend/src/hooks/useAuth.ts
+++ EjercicioLoginNeitekFrontend/src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5134/api/auth";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5134/api/auth";
 
 interface SignupData {
   email: string;
